9049a3b888a22c502703f2cb5142a007129a190c
astvu24524522u09026 <144410535+astvu24524522u09026@users.noreply.github.com>
2023-09-21 14:02:50 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,35 +1,48 @@
-
-# Python program to create Bankaccount class
-# with both a deposit() and a withdraw() function
-class Bank_Account:
-
-  def __init__(self):
-    self.balance = 0
-    print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
-
-  def deposit(self):
-    amount = float(input("Enter amount to be Deposited: "))
-    self.balance += amount
-    print("\n Amount Deposited:", amount)
-
-  def withdraw(self):
-    amount = float(input("Enter amount to be Withdrawn: "))
-    if self.balance >= amount:
-      self.balance -= amount
-      print("\n You Withdrew:", amount)
-    else:
-      print("\n Insufficient balance  ")
-
-  def display(self):
-    print("\n Net Available Balance=", self.balance)
-
-
-# Driver code
-
-# creating an object of class
-s = Bank_Account()
-
-# Calling functions with that class object
-s.deposit()
-s.withdraw()
-s.display()
+// C++ program to calculate the CGPA
+// and CGPA percentage of a student
+#include<bits/stdc++.h>
+using namespace std;
+ 
+double CgpaCalc(double marks[], int n)
+{
+    // Variable to store the grades in
+    // every subject
+    double grade[n];
+ 
+    // Variables to store CGPA and the
+    // sum of all the grades
+    double cgpa, sum = 0;
+ 
+    // Computing the grades
+    for(int i = 0; i < n; i++)
+    {
+       grade[i] = (marks[i] / 10);
+    }
+ 
+    // Computing the sum of grades
+    for(int i = 0; i < n; i++)
+    {
+       sum += grade[i];
+    }
+ 
+    // Computing the CGPA
+    cgpa = sum / n;
+ 
+    return cgpa;
+}
+ 
+// Driver code
+int main()
+{
+    int n = 5;
+    double marks[] = { 90, 80, 70, 80, 90 };
+ 
+    double cgpa = CgpaCalc(marks, n);
+         
+    cout << "CGPA = ";
+    printf("%.1f\n", cgpa);
+    cout << "CGPA Percentage = ";
+    printf("%.2f", cgpa * 9.5);
+}
+ 
+// This code is contributed by Bhupendra_Singh
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-# Define the Player class
-class Player:
-    def play(self):
-        print("The player is playing cricket")
-
-# Define the Batsman class that inherits from Player
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting")
-
-# Define the Bowler class that inherits from Player
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling")
-
-# Create objects of the Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
-
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+import re
+ 
+# Sample input
+arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+x = 110
+ 
+# Convert the array to a string
+arr_str = ','.join(str(i) for i in arr)
+ 
+# Use regular expression to search for the element in the string
+match = re.search(r'\b{}\b'.format(x), arr_str)
+ 
+# Output
+if match:
+    # Calculate the index by counting the number of commas before the match
+    result = arr_str[:match.start()].count(',')
+    print(f"Element {x} is present at index {result}")
+else:
+    print(f"Element {x} is not present in the array")
